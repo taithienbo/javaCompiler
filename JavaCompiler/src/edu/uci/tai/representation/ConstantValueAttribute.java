a960b5cb475dc969f0636de7f6e8eedae6a47111
Tai Bo <taithienbo@gmail.com>
2013-02-20 00:00:25 -0800

    implemented ConstantValueAttribute

--- JavaCompiler/src/edu/uci/tai/representation/ConstantValueAttribute.java
+++ JavaCompiler/src/edu/uci/tai/representation/ConstantValueAttribute.java
@@ -3,14 +3,40 @@ package edu.uci.tai.representation;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import edu.uci.tai.constantPool.Structure;
+
 public class ConstantValueAttribute extends Attribute 
 {
-
+	private static final int CONSTANT_VALUE_INDEX_NUM_BYTES = 2;
+	private int constantValueIndex;
+	
 	public ConstantValueAttribute(FileInputStream fis) throws IOException 
 	{
 		super(fis);
-		
 		setName("ConstantValue");
+		initialize();
+	}
+	
+	private void initialize() throws IOException
+	{
+		byte[] consValueIndexBytes = new byte[CONSTANT_VALUE_INDEX_NUM_BYTES];
+		fis.read(consValueIndexBytes);
+		
+		constantValueIndex = (int) Structure.valueFromBytes(consValueIndexBytes);
+	}
+	
+	public int consValueIndex()
+	{
+		return constantValueIndex;
+	}
+	
+	@Override
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder(super.toString());
+		builder.append(String.format("constant_value_index", consValueIndex())).append("\n");
+		
+		return builder.toString();
 	}
 
 }
